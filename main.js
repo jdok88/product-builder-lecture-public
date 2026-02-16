f4d7eb9581a342a0edb654db6087e68e82fb8b73
hahuhejung1988 <hahuhejung1988@gmail.com>
2026-02-16 08:13:25 +0000

    fix: broken images

--- main.js
+++ main.js
@@ -6,13 +6,13 @@ const body = document.body;
 
 const dinnerMenus = [
     { name: "치킨", imageUrl: "https://images.unsplash.com/photo-1562967914-608f82629710?q=80&w=2073&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
-    { name: "피자", imageUrl: "https://images.unsplash.com/photo-1513104890138-e1f88c70de1c?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+    { name: "피자", imageUrl: "https://source.unsplash.com/400x300/?pizza" },
     { name: "햄버거", imageUrl: "https://images.unsplash.com/photo-1571091718767-18b5b1457add?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
-    { name: "떡볶이", imageUrl: "https://images.unsplash.com/photo-1628423640029-a7f43b235d9a?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+    { name: "떡볶이", imageUrl: "https://source.unsplash.com/400x300/?tteokbokki" },
     { name: "라면", imageUrl: "https://images.unsplash.com/photo-1552611052-33e04de081de?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
-    { name: "김치찌개", imageUrl: "https://upload.wikimedia.org/wikipedia/commons/a/a5/Korean.cuisine-Kimchi_jjigae-01.jpg" },
-    { name: "된장찌개", imageUrl: "https://images.unsplash.com/photo-1598246083329-a86842226e67?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
-    { name: "삼겹살", imageUrl: "https://images.unsplash.com/photo-1597318181409-cf299f113a83?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+    { name: "김치찌개", imageUrl: "https://plus.unsplash.com/premium_photo-1669687759596-8d6d8836f480?crop=entropy&cs=srgb&fm=jpg&ixid=M3wxMjA3fDB8MXxzZWFyY2h8MXx8a2ltY2hpJTIwamppZ2FlfGVufDB8fHx8MTc3MTEyOTc1N3ww&ixlib=rb-4.1.0&q=85" },
+    { name: "된장찌개", imageUrl: "https://plus.unsplash.com/premium_photo-1705406169429-df5950bc2995?ixid=M3wxMjA3fDB8MXxzZWFyY2h8MXx8amppZ2FlfGVufDB8fHx8MTc3MTIyODY4Nnww&ixlib=rb-4.1.0" },
+    { name: "삼겹살", imageUrl: "https://source.unsplash.com/400x300/?samgyeopsal" },
     { name: "초밥", imageUrl: "https://images.unsplash.com/photo-1579584425555-c3ce17fd4351?q=80&w=1948&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
     { name: "파스타", imageUrl: "https://images.unsplash.com/photo-1579684947550-22e945225d9a?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" }
 ];
